ff5e8728a865121299c412fe075c494c9801d6fb
saurabhrajankar <saurabhrajankar777@gmail.com>
2023-04-05 13:43:54 +0530

    Responceive Login and sign up page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 
 
 @NgModule({
@@ -21,7 +22,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatCardModule
+    MatCardModule,
+    MatSlideToggleModule
  
   ],
   providers: [],
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
+ // on:any="off";
   constructor() { }
 
   ngOnInit(): void {
   }
+  // OnMethod(){
+   
+  //   if(this.on=="off")
+  //   {
+  //     this.on="on"
+  //     this.on="Rember Me"
+  //   }
+  //   else
+  //   {
+  //     this.on="off"
+  //   }
+  }
 
-}
+
